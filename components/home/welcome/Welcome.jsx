792e7c98347f68a9ecf9931999bb8e71c7f51a6d
saulkurosaki <saulkurosaki13@gmail.com>
2024-05-22 11:15:07 -0600

    feat(Welcome): implement the Welcome component userName display

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -17,7 +17,10 @@ const Welcome = () => {
 
   return (
     <View>
-      <View style={styles.container}></View>
+      <View style={styles.container}>
+        <Text style={styles.userName}>Hello Saul</Text>
+        {/* <Text style={styles.welcomeMessage}>Find your perfect job</Text> */}
+      </View>
     </View>
   );
 };
