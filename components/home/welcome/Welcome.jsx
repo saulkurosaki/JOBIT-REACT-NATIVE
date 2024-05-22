6f49f0913f5e97b9e8d5dea097d1bfebdce25a82
saulkurosaki <saulkurosaki13@gmail.com>
2024-05-22 11:17:33 -0600

    feat(Welcome): implement the TextInput component container initial layout

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -21,6 +21,14 @@ const Welcome = () => {
         <Text style={styles.userName}>Hello Saul</Text>
         <Text style={styles.welcomeMessage}>Find your perfect job</Text>
       </View>
+
+      <View style={styles.searchContainer}>
+        <View style={styles.searchWrapper}>
+          {/* <TextInput
+            style={styles.searchInput}
+          /> */}
+        </View>
+      </View>
     </View>
   );
 };
