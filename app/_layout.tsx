2f676ab0855fdd6b1b7f230f87d60f0e187966df
saulkurosaki <saulkurosaki13@gmail.com>
2024-05-22 11:24:04 -0600

    feat(_layout.tsx): implement all the necessary fonts

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -6,6 +6,18 @@ import * as SplashScreen from "expo-splash-screen";
 SplashScreen.preventAutoHideAsync();
 
 const Layout = () => {
+  const [fontsLoaded] = useFonts({
+    DMBold: require("../assets/fonts/DMSans-Bold.ttf"),
+    DMMedium: require("../assets/fonts/DMSans-Medium.ttf"),
+    DMRegular: require("../assets/fonts/DMSans-Regular.ttf"),
+  });
+
+  // const onLayoutRootView = useCallback(async () => {
+  //   if(fontsLoaded) {
+  //     SplashScreen.hideAsync();
+  //   }
+  // }, [fontsLoaded]);
+
   return <Stack />;
 };
 
