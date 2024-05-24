9271a2e0eb87214b9b941b5b07717a59024e77f5
saulkurosaki <saulkurosaki13@gmail.com>
2024-05-23 20:48:07 -0600

    feat(PopularJobs): implement the call to the useFetch custom hook

--- hook/useFetch.js
+++ hook/useFetch.js
@@ -1,10 +1,7 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-import { RAPID_API_KEY } from "@env";
 
-const rapidApiKey = RAPID_API_KEY;
-
-const useFetch = (endpoint, query) => {
+export const useFetch = (endpoint, query) => {
   const [data, setData] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
@@ -13,7 +10,7 @@ const useFetch = (endpoint, query) => {
     method: "GET",
     url: `https://jsearch.p.rapidapi.com/${endpoint}`,
     headers: {
-      "x-rapidapi-key": rapidApiKey,
+      "x-rapidapi-key": "f2475a1f79mshc0533298d992273p1b4b04jsn02fa86c6678a",
       "x-rapidapi-host": "jsearch.p.rapidapi.com",
     },
     params: { ...query },
