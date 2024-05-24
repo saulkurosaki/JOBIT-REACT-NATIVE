e019d2f8a36bc42643f8b342899fc53c06eb2498
saulkurosaki <saulkurosaki13@gmail.com>
2024-05-23 19:59:10 -0600

    feat(PopularJobs): implement some aditional props to the job cards

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -31,7 +31,13 @@ const Popularjobs = () => {
         ) : error ? (
           <Text>Something went wrong</Text>
         ) : (
-          <FlatList data={[1, 2, 3, 4, 5, 6, 7, 8, 9]} />
+          <FlatList
+            data={[1, 2, 3, 4, 5, 6, 7, 8, 9]}
+            renderItem={({ item }) => <PopularJobCard item={item} />}
+            keyExtractor={(item) => item?.job_id}
+            contentContainerStyle={{ columnGap: SIZES.medium }}
+            horizontal
+          />
         )}
       </View>
     </View>
