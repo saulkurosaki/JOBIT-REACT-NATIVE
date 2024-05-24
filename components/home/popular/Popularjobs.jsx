1509e54a69e5c51e1904e1951e171725bbf77712
saulkurosaki <saulkurosaki13@gmail.com>
2024-05-23 19:48:14 -0600

    feat(PopularJobs): implement the show all button

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -17,6 +17,9 @@ const Popularjobs = () => {
     <View style={styles.container}>
       <View style={styles.header}>
         <Text style={styles.headerTitle}>Popular Jobs</Text>
+        <TouchableOpacity>
+          <Text style={styles.headerBtn}>Show All</Text>
+        </TouchableOpacity>
       </View>
     </View>
   );
