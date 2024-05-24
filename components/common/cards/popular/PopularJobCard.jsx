255244d88c36f84429f8a4ff2cfd200b766ee0a2
saulkurosaki <saulkurosaki13@gmail.com>
2024-05-23 20:59:02 -0600

    feat(PopularJobCard): implement the TouchableOpacity component styles along as the popular jobs image

--- components/common/cards/popular/PopularJobCard.jsx
+++ components/common/cards/popular/PopularJobCard.jsx
@@ -7,7 +7,15 @@ const PopularJobCard = ({ item, selectedJob, handleCardPress }) => {
     <TouchableOpacity
       style={styles.container(selectedJob, item)}
       onPress={() => handleCardPress(item)}
-    ></TouchableOpacity>
+    >
+      <TouchableOpacity style={styles.logoContainer(selectedJob, item)}>
+        <Image
+          source={{ uri: item.employer_logo }}
+          resizeMode="contain"
+          style={styles.logoImage}
+        />
+      </TouchableOpacity>
+    </TouchableOpacity>
   );
 };
 
