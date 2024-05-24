ddaeb78ebd99d75ce4b54660805962cb82c3d005
saulkurosaki <saulkurosaki13@gmail.com>
2024-05-24 13:37:34 -0600

    feat(JobDetails): implement all the component necessary imports

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -1,4 +1,24 @@
-import { View, Text } from "react-native";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  ScrollView,
+  ActivityIndicator,
+  RefreshControl,
+} from "react-native";
+import { Stack, useRouter, useSearchParams } from "expo-router";
+import { useCallback, useState } from "react";
+
+import {
+  Company,
+  JobAbout,
+  JobFooter,
+  JobTabs,
+  ScreenHeaderBtn,
+  Specifics,
+} from "../../components";
+import { COLORS, icons, SIZES } from "../../constants";
+import { useFetch } from "../../hook/useFetch";
 
 const JobDetails = () => {
   return (
