946e21f405e88396ae42f37e3b00324251b67f1c
saulkurosaki <saulkurosaki13@gmail.com>
2024-05-25 09:03:28 -0600

    feat(Company): implement the component company location display

--- components/jobdetails/company/Company.jsx
+++ components/jobdetails/company/Company.jsx
@@ -25,6 +25,14 @@ const Company = ({ companyLogo, jobTitle, companyName, location }) => {
 
       <View style={styles.companyInfoBox}>
         <Text style={styles.companyName}>{companyName} / </Text>
+        <View style={styles.locationBox}>
+          <Image
+            source={icons.location}
+            resizeMode="contain"
+            style={styles.locationImage}
+          />
+          <Text style={styles.locationName}>{location}</Text>
+        </View>
       </View>
     </View>
   );
