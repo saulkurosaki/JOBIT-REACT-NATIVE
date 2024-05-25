a0bb1095234b3f755aaeea9adb2552b51597bc1a
saulkurosaki <saulkurosaki13@gmail.com>
2024-05-25 08:56:09 -0600

    feat(Company): implement the component company image display

--- components/jobdetails/company/Company.jsx
+++ components/jobdetails/company/Company.jsx
@@ -8,7 +8,16 @@ import { checkImageURL } from "../../../utils";
 const Company = ({ companyLogo, jobTitle, companyName, location }) => {
   return (
     <View style={styles.container}>
-      <View style={styles.logoBox}></View>
+      <View style={styles.logoBox}>
+        <Image
+          source={{
+            uri: checkImageURL(companyLogo)
+              ? companyLogo
+              : "https://t4.ftcdn.net/jpg/05/05/61/73/360_F_505617309_NN1CW7diNmGXJfMicpY9eXHKV4sqzO5H.jpg",
+          }}
+          style={styles.logoImage}
+        />
+      </View>
     </View>
   );
 };
